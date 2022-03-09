a9e8f423abdc3864853a246ae29a11e8c8a9858b
Tarabas Daniel <tarabas-florentin.tarabas@student.tuiasi.ro>
2022-03-09 12:45:14 +0200

    commit 1

---
+++ src/Teacher.java
@@ -0,0 +1,12 @@
+
+public class Teacher {
+	Teacher(){
+		System.out.println("Ctr. din Teacher");
+	}
+	
+	@Override
+	public String toString() {
+
+		return "from Teacher";
+	}
+}
---
+++ src/TestStudentApp.java
@@ -0,0 +1,24 @@
+
+public class TestStudentApp {
+	public static void main(String[] args) {
+		System.out.println("Main for Student App project");
+		//declares a student
+		Student s1 = new Student(10,"Popescu","Ionut");
+		Student s2 = new Student(10,"Ionescu","Raluca");
+		Student s3 = new Student(9,"Aaa","B");
+
+		//Student s3 = new Student(9,"ANC","Raluca");
+		Teacher t1 = new Teacher();
+		Teacher t2 = new Teacher(); //222
+		Teacher t3 = new Teacher(); //222
+		
+		
+		
+		System.out.println(s1);
+		s1.print3();
+		s2.print3();
+		
+		System.out.println(t1);
+		
+	}
+}
